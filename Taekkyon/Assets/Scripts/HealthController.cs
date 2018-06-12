5aeaad15563df06f72d98a1ca07686b805b8f47e
mingyu0403 <jmg6017@naver.com>
2018-06-12 21:13:04 +0900

    first commit

---
+++ Taekkyon/Assets/Scripts/HealthController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class HealthController : NetworkBehaviour
+{
+
+    public const int maxHealth = 100;
+
+    public Slider healthSliderA;
+    public Slider healthSliderB;
+    Slider healthSlider;
+
+    // [SyncVar] : 네트워크를 넘나들며 수치를 자동으로 동기화 해준다.
+    // hook : TakeDamage가 발동될 때마다 훅이 걸려 자동으로 실행된다.
+    [SyncVar(hook = "OnChangeHealth")]
+
+    public int currentHealth = maxHealth;
+
+    void Start()
+    {
+        if (isServer)
+        {
+            healthSlider = healthSliderA;
+            Debug.Log("A");
+        } else
+        {
+            healthSlider = healthSliderB;
+            Debug.Log("B");
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            // 피 다 닳음
+        }
+    }
+
+    void OnChangeHealth(int health)
+    {
+        healthSlider.value = health;
+    }
+}
---
+++ Taekkyon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public enum PlayerStatement { idle, walk, attack, hit, defend, die };
+    public PlayerStatement MyState = PlayerStatement.idle;
+
+    void Start()
+    {
+        if (!isServer)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
+        }
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) { MyState = PlayerStatement.attack; }
+        if (Input.GetKeyDown(KeyCode.D)) { MyState = PlayerStatement.defend; }
+
+        switch (MyState)
+        {
+            case PlayerStatement.attack : break;
+            case PlayerStatement.defend : break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("들왔다");
+        var health = collider.GetComponent<HealthController>();
+
+        if (health != null)
+        {
+            Debug.Log("깍는다");
+            health.TakeDamage(10);
+        }
+    }
+}
